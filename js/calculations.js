8cd21e3c314b1feb789646fa91717087ff7e7607
Nidheesh Puliyath <48721587+nidheesh65@users.noreply.github.com>
2026-02-15 13:40:00 -0800

    Update calculations.js

--- js/calculations.js
+++ js/calculations.js
@@ -1,1 +1,7 @@
-
+export const Finance = {
+    getSWR: (nw, rate) => ({ monthly: (nw * rate * 0.04) / 12, annual: (nw * rate * 0.04) }),
+    getFV: (m, r, y) => {
+        const rate = (r / 100) / 12;
+        return m * ((Math.pow(1 + rate, y * 12) - 1) / rate);
+    }
+};
