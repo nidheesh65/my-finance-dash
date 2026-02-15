46a0e9de377f6e10f10106e388880e062b75e019
Nidheesh Puliyath <48721587+nidheesh65@users.noreply.github.com>
2026-02-15 13:39:29 -0800

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,5 @@
-
+export const Storage = {
+    save: (key, val) => localStorage.setItem(key, JSON.stringify(val)),
+    get: (key) => JSON.parse(localStorage.getItem(key)),
+    clear: () => { localStorage.clear(); location.reload(); }
+};
